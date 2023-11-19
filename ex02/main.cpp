99db71c2e96699b746517aabd39b953c5344bd02
Salah <salah.bouheniche@gmail.com>
2023-11-19 12:35:08 +0100

    Complete ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 12:17:27 by sbouheni          #+#    #+#             */
+/*   Updated: 2023/11/19 12:28:03 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Address of string variable: " << &str << std::endl;
+	std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "Value of strind variable: " << str << std::endl;
+	std::cout << "Value held by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value held by stringREF: " << stringREF << std::endl;
+	return (0);
+}
